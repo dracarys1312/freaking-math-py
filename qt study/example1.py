eb96cfe3bd15d78c926725c66b07777dec49cfbe
Pt <dracarys1312@gmail.com>
2016-08-30 20:21:24 +0700

    :bookmark: 0.1.0

--- qt study/example1.py
+++ qt study/example1.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     w = QWidget()
-    w.resize(250, 150)
+    w.resize(500, 200)
     w.move(300, 300)
     w.setWindowTitle('Simple')
     w.show()
